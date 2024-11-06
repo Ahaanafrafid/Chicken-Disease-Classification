b71b95517f4cfe591a73e73ae740536fe2b2077b
Ahaanafrafid <ahanafrafidhoque@gmail.com>
2024-11-06 13:57:40 -0500

    Base Model Prep done

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from src.ChickenDiseaseClassification import logger  # Ensure the import path is correct
 from src.ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.ChickenDiseaseClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        data_ingestion = DataIngestionTrainingPipeline()
-        data_ingestion.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-    except Exception as e:
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/ChickenDiseaseClassification/entity/config_entity.py
+++ src/ChickenDiseaseClassification/entity/config_entity.py
@@ -8,3 +8,15 @@ class DataIngestionConfig:
         self.source_URL = source_URL
         self.local_data_file = local_data_file
         self.unzip_dir = unzip_dir
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: tuple
+    params_learning_rate: float
+    params_include_top: bool
+    params_weight: str
+    params_classes: int

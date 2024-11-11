f26dffade2795be106f99124d28ffc1215d13cf4
Ahaanafrafid <ahanafrafidhoque@gmail.com>
2024-11-11 14:33:06 -0500

    dvc pipeline completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.ChickenDiseaseClassification import logger  # Ensure the import path is
 from src.ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.ChickenDiseaseClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.ChickenDiseaseClassification.pipeline.stage_03_training import ModelTrainingPipeline
-
+from src.ChickenDiseaseClassification.pipeline.stage_04_evaluation import EvaluationPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -31,6 +31,18 @@ if __name__ == '__main__':
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    except Exception as e:
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"   
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
     except Exception as e:
--- src/ChickenDiseaseClassification/config/configuration.py
+++ src/ChickenDiseaseClassification/config/configuration.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 from src.ChickenDiseaseClassification.constants import *
 from src.ChickenDiseaseClassification.utils.common import read_yaml, create_directories
-from src.ChickenDiseaseClassification.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig)
+from src.ChickenDiseaseClassification.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig,EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -87,3 +88,13 @@ class ConfigurationManager:
       )
 
       return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+        path_of_model=Path("artifacts/training/model.h5"),
+        training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+        all_params=self.params,
+        params_image_size=self.params.IMAGE_SIZE,
+        params_batch_size=self.params.BATCH_SIZE
+      )
+        return eval_config

e2f592b14ee124d1992bdda1715548bc5325f314
Ahaanafrafid <ahanafrafidhoque@gmail.com>
2024-11-11 12:24:30 -0500

    Training added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.ChickenDiseaseClassification import logger  # Ensure the import path is correct
 from src.ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.ChickenDiseaseClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from src.ChickenDiseaseClassification.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -23,4 +23,15 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
+        raise e
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    except Exception as e:
         raise e
